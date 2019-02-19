f82bd7f9aec930ef6f906fb8e63836f86b805455
Muhammad Hoti <muhammad.hoti@gmail.com>
2019-02-20 01:36:44 +0500

    map done

--- screens/info.js
+++ screens/info.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import { View , Button, Text, StyleSheet, Image, Platform} from 'react-native';
-import { Container, Header, Content, Form, Item, Input, Picker, Icon  } from 'native-base';
-import { ImagePicker } from 'expo';
+import { Container, Header, Content, Form, Item, Input, Picker, Icon, TouchableOpacity  } from 'native-base';
+import { ImagePicker, MapView } from 'expo';
 import firebase from '../config/firebase.js';
-// import RNFetchBlob from 'react-native-fetch-blob'
-
-// const Blob = RNFetchBlob.polyfill.Blob
-// const fs = RNFetchBlob.fs
-// window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
-// window.Blob = Blob
 
 export default class Info extends React.Component {
   constructor(props){
@@ -16,8 +10,10 @@ export default class Info extends React.Component {
     this.state={
       image: null,
       selected2: undefined,
+      mapComponent : false,
     }
     this.uploadImageAsync = this.uploadImageAsync.bind(this)
+    this.showMap = this.showMap.bind(this)
   }
 
   componentDidMount(){
@@ -31,6 +27,22 @@ export default class Info extends React.Component {
     userData.profession = value
     this.setState(userData);
     this.setState({selected2 : value})
+  }
+
+  _pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [3, 3],
+    });
+
+    if (!result.cancelled) {
+      this.setState({ image: result.uri });
+      const uri = result.uri;
+      const {userData} = await this.state;
+      const displayPicture = await this.uploadImageAsync(uri);
+      userData.displayPicture = await displayPicture
+      await this.setState(displayPicture);
+    }
   }
 
    async uploadImageAsync(uri) {
@@ -54,86 +66,99 @@ export default class Info extends React.Component {
       .child(new Date().toString());
     const snapshot = await  ref.put(blob);
   
-    // We're done with the blob, close and release it
     blob.close();
     console.log(snapshot.ref.getDownloadURL())
     return await snapshot.ref.getDownloadURL();
   }
 
-
-  _pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true,
-      aspect: [3, 3],
-    });
-
-    if (!result.cancelled) {
-      this.setState({ image: result.uri });
-      const uri = result.uri;
-      const displayPicture = await this.uploadImageAsync(uri);
-      const {userData} = await this.state;
-      userData.displayPicture = await displayPicture
-      await this.setState(displayPicture);
-    }
+  showMap(){
+    this.setState({mapComponent : true})
   }
-
 
   render() {
     console.log(this.state.userData)
-    let { image } = this.state;
+    let { image, mapComponent } = this.state;
     return (
       <View style={{flex: 1}}>
-        <Container>
-        <Header />
-        <Content>
-          <Text style={styles.titleText}>Enter Your Information</Text>
-          <Form>
-          <Item floatingLabel>
-              <Input 
-              placeholder = "Enter Your Phone Number"
-              onChange={
-                (e)=>{
-                  const {userData} = this.state
-                  userData.number = e.nativeEvent.text
-                  this.setState(userData)
-              }
-              }
-              />
-            </Item>
-            <Item picker>
-            <Text style={styles.titleText2}>Select Any One</Text>
-              <Picker
-                mode="dropdown"
-                iosIcon={<Icon name="arrow-down" />}
-                style={{ width: undefined }}
-                placeholder="Select your Profession"
-                placeholderStyle={{ color: "#bfc6ea" }}
-                placeholderIconColor="#007aff"
-                selectedValue={this.state.selected2}
-                onValueChange={this.onValueChange2.bind(this)}
-              >
-                <Picker.Item label="Electrician" value="Electrician" />
-                <Picker.Item label="Plumber" value="Plumber" />
-                <Picker.Item label="Carpenter" value="Carpenter" />
-                <Picker.Item label="Colorist" value="Colorist" />
-                <Picker.Item label="Welder" value="Welder" />
-                <Picker.Item label="Looking For Handy Man" value="Looking For Handy Man" />
-              </Picker>
-            </Item>
-            <Item>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        {image &&
-          <Image source={{ uri: image }} style={{ width: 50, height: 50 }} />}
-        <Button
-          title="Upload Your Picture"
-          onPress={this._pickImage}
-        />
-      </View>
-            </Item>
-          </Form>
-        </Content>
-      </Container>
-      </View>
+          {!mapComponent &&
+            <Container>
+            <Header />
+            <Content>
+              <Text style={styles.titleText}>Enter Your Information</Text>
+              <Form>
+              <Item floatingLabel>
+                  <Input 
+                  placeholder = "Enter Your Phone Number"
+                  onChange={
+                    (e)=>{
+                      const {userData} = this.state
+                      userData.number = e.nativeEvent.text
+                      this.setState(userData)
+                  }
+                  }
+                  />
+                </Item>
+                <Item picker>
+                <Text style={styles.titleText2}>Select Any One</Text>
+                  <Picker
+                    mode="dropdown"
+                    iosIcon={<Icon name="arrow-down" />}
+                    style={{ width: undefined }}
+                    placeholder="Select your Profession"
+                    placeholderStyle={{ color: "#bfc6ea" }}
+                    placeholderIconColor="#007aff"
+                    selectedValue={this.state.selected2}
+                    onValueChange={this.onValueChange2.bind(this)}
+                  >
+                    <Picker.Item label="Electrician" value="Electrician" />
+                    <Picker.Item label="Plumber" value="Plumber" />
+                    <Picker.Item label="Carpenter" value="Carpenter" />
+                    <Picker.Item label="Colorist" value="Colorist" />
+                    <Picker.Item label="Welder" value="Welder" />
+                    <Picker.Item label="Looking For Handy Man" value="Looking For Handy Man" />
+                  </Picker>
+                </Item>
+                <Item>
+                  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                  {image &&
+                      <Image  source={{ uri: image }} style={{ width: 50, height: 50 }} />
+                  }
+                  {!image &&
+                    <Button
+                      title="Upload Your Picture"
+                      onPress={this._pickImage}
+                    />
+                  }
+                </View>
+                </Item>
+                <Item>
+                  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                  {!mapComponent &&
+                    <Button
+                      title="Set Your Location"
+                      onPress={this.showMap}
+                    />
+                  }
+                </View>
+                </Item>
+              </Form>
+            </Content>
+          </Container>
+          }
+          {mapComponent &&
+                    <MapView
+                    style={{
+                      flex: 1
+                    }}
+                    initialRegion={{
+                      latitude: 24.8607,
+                      longitude: 67.0011,
+                      latitudeDelta: 0.0922,
+                      longitudeDelta: 0.0421
+                    }}
+                  />
+                  }        
+        </View>
     );
   }
 }
@@ -149,5 +174,11 @@ const styles = StyleSheet.create({
   titleText2: {
     fontSize: 10,
     fontWeight: 'bold',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
